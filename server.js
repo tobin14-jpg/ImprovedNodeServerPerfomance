e43d84c1cff9f5005105e591b60cbbd2d88cbc9d
Ice Tea <shauntobin88@gmail.com>
2022-09-05 21:07:26 +0800

    maximized server performance based off amount of CPU cores available on the machine

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 
 const express = require('express');
 const cluster = require('cluster');
+const os = require('os');
 
 
 const app = express();
 
 function delay(duration) {
   const startTime = Date.now();
-  while(Date.now() - startTime < duration) {
+  while (Date.now() - startTime < duration) {
     // the event loop is now blocked...
   }
 }
@@ -22,12 +23,14 @@ app.get('/timer', (req, res) => {
   res.send(`The alarm bells are ringing!: ${process.pid}`);
 })
 
-if(cluster.isMaster) {
-    console.log('Master has been started');
+if (cluster.isMaster) {
+  console.log('Master has been started');
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
     cluster.fork();
-    cluster.fork();
-}   else {
-    console.log('Worker process has been started');
-    app.listen(3000);
+  }
+} else {
+  console.log('Worker process has been started');
+  app.listen(3000);
 }
 
