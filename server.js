c6a2ea459a28064162bc1d981c1069822b265276
Ice Tea <shauntobin88@gmail.com>
2022-09-05 20:55:35 +0800

    built cluster to optimize response time performance on multiple tabs

---
+++ server.js
@@ -0,0 +1,33 @@
+// response times should be kept under 100/200 ms
+
+const express = require('express');
+const cluster = require('cluster');
+
+
+const app = express();
+
+function delay(duration) {
+  const startTime = Date.now();
+  while(Date.now() - startTime < duration) {
+    // the event loop is now blocked...
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send(`Performance example: ${process.pid}`);
+});
+
+app.get('/timer', (req, res) => {
+  delay(9000);
+  res.send(`The alarm bells are ringing!: ${process.pid}`);
+})
+
+if(cluster.isMaster) {
+    console.log('Master has been started');
+    cluster.fork();
+    cluster.fork();
+}   else {
+    console.log('Worker process has been started');
+    app.listen(3000);
+}
+
